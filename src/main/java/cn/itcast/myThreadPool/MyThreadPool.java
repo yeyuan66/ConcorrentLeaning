014b527c21d7c8462646b9fd32472a4ebca9d495
ye-yuan666 <1992524640@qq.com>
2021-11-19 00:58:11 +0800

    完成线程池成员变量编写和构造，以及内部worker类编写
    待完成：线程池执行任务方法

--- src/main/java/cn/itcast/myThreadPool/MyThreadPool.java
+++ src/main/java/cn/itcast/myThreadPool/MyThreadPool.java
@@ -1,16 +1,31 @@
 package cn.itcast.myThreadPool;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+@Slf4j(topic = "c.MyThreadPool")
 public class MyThreadPool {
+}
+
+//拒绝策略接口，策略模式思想
+@FunctionalInterface
+interface RejectPolicy<T>{
+    void reject(BlockingQueue<T>queue,T task);
 }
 
 //自定义阻塞队列
 class BlockingQueue<T>{
+    public BlockingQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
     //队列
     Deque<T> queue = new ArrayDeque<>();
     //最大容量
@@ -115,6 +130,61 @@ class BlockingQueue<T>{
     }
 
 
+}
+@Slf4j(topic = "c.ThreadPool")
+class ThreadPool{
+    //阻塞队列
+    private BlockingQueue<Runnable> blockingQueue;
+    //任务集合 ，并发集合应该是final的
+
+    private final Set<Worker> workerSet = new HashSet<>();
+    //核心线程数
+    private int coreSize;
+    //最长等待时间
+    private long TimeOut;
+    //时间单位
+    private TimeUnit timeUnit;
+
+    private RejectPolicy<Runnable> rejectPolicy;
+    public ThreadPool(int queueSize, int coreSize, long timeOut, TimeUnit timeUnit,RejectPolicy<Runnable> rejectPolicy) {
+        this.blockingQueue = new BlockingQueue<>(queueSize);
+        this.coreSize = coreSize;
+        TimeOut = timeOut;
+        this.timeUnit = timeUnit;
+        this.rejectPolicy = rejectPolicy;
+    }
+
+    //TODO 线程池执行任务方法
+
+
+    class Worker extends Thread{
+         Runnable task;
+        Worker(Runnable task){
+            this.task = task;
+        }
+
+        @Override
+        public void run() {
+            //当前没有任务，从阻塞队列中获取
+            while (task!=null || (task = blockingQueue.take())!=null){
+                try{
+                    log.debug("线程运行中：{}",task);
+                    task.run();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    task =null;
+                }
+            }
+
+            synchronized (workerSet){
+                log.debug("{}从workers中移除",this);
+                workerSet.remove(this);
+            }
+
+
+        }
+    }
 }
 
 
