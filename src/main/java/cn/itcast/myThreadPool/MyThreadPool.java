51b9bd1978421f583782f6f014e0bd6500008721
ye-yuan666 <1992524640@qq.com>
2021-11-20 00:55:51 +0800

    完成： 线程池执行方法，任务队列满时根据自己的策略进行处理，并测试第一种策略：死等
    待完成：其他策略测试

--- src/main/java/cn/itcast/myThreadPool/MyThreadPool.java
+++ src/main/java/cn/itcast/myThreadPool/MyThreadPool.java
@@ -12,6 +12,33 @@ import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j(topic = "c.MyThreadPool")
 public class MyThreadPool {
+
+    public static void main(String[] args) {
+        ThreadPool threadPool = new ThreadPool(1,
+                2,1, TimeUnit.MILLISECONDS,  (queue, task)->{
+// 1. 死等
+ queue.put(task);
+// 2) 带超时等待
+// queue.offer(task, 1500, TimeUnit.MILLISECONDS);
+// 3) 让调用者放弃任务执行
+// log.debug("放弃{}", task);
+// 4) 让调用者抛出异常
+// throw new RuntimeException("任务执行失败 " + task);
+// 5) 让调用者自己执行任务
+  //          task.run();
+        });
+        for (int i = 0; i < 4; i++) {
+            int j = i;
+            threadPool.execute(() -> {
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                log.debug("{}", j);
+            });
+        }
+    }
 }
 
 //拒绝策略接口，策略模式思想
@@ -21,6 +48,7 @@ interface RejectPolicy<T>{
 }
 
 //自定义阻塞队列
+@Slf4j(topic = "c.BlockingQueue")
 class BlockingQueue<T>{
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
@@ -44,15 +72,16 @@ class BlockingQueue<T>{
        try{
            while (queue.isEmpty()){
                try {
-                   fullWaitSet.await();
+                   emptyWaitSet.await();
                } catch (InterruptedException e) {
                    e.printStackTrace();
                }
            }
            //获取队列的队头元素
            T t = queue.removeFirst();
+           log.debug("从任务队列移除： {} ...", t);
            //有元素，增加条件变量
-           emptyWaitSet.signal();
+           fullWaitSet.signal();
            return t;
        }finally {
            lock.unlock();
@@ -71,15 +100,16 @@ class BlockingQueue<T>{
                 if(nanos <= 0)return null;
                 try {
                     //边等待边计算剩余时间
-                    nanos = fullWaitSet.awaitNanos(nanos);
+                    nanos = emptyWaitSet.awaitNanos(nanos);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+
             //获取队列的队头元素
             T t = queue.removeFirst();
             //有元素，增加条件变量
-            emptyWaitSet.signal();
+            fullWaitSet.signal();
             return t;
         }finally {
             lock.unlock();
@@ -94,12 +124,14 @@ class BlockingQueue<T>{
         try {
             while (queue.size() == capacity){
                 try {
+                    log.debug("等待加入任务队列 {} ...", t);
                     fullWaitSet.await();
                 }catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 
             }
+            log.debug("加入任务队列 {}", t);
             queue.addLast(t);
             emptyWaitSet.signal();
         }  finally {
@@ -115,12 +147,14 @@ class BlockingQueue<T>{
             while (queue.size() == capacity){
                 if(nanos <= 0)return false;
                 try {
+                    log.debug("等待加入任务队列 {} ...", t);
                     nanos = fullWaitSet.awaitNanos(nanos);
                 }catch (InterruptedException e){
                     e.printStackTrace();
                 }
 
             }
+            log.debug("加入任务队列 {}", t);
             queue.addLast(t);
             emptyWaitSet.signal();
             return true;
@@ -129,6 +163,25 @@ class BlockingQueue<T>{
         }
     }
 
+    //设置策略的队列添加
+    public void tryPut(RejectPolicy<T> rejectPolicy,T task){
+        lock.lock();
+        try {
+            if(queue.size() < capacity){
+                queue.addLast(task);
+                emptyWaitSet.signal();
+            }
+            else {
+                log.debug("加入任务队列 {}", task);
+                rejectPolicy.reject(this,task);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+
+    }
 
 }
 @Slf4j(topic = "c.ThreadPool")
@@ -157,9 +210,34 @@ class ThreadPool{
     //TODO 线程池执行任务方法
 
 
+    public void execute(Runnable task){
+        //如果当前核心数未达到上限，创建核心线程
+        //必须互斥操作核心线程集合，可以用并发集合优化
+        synchronized (workerSet){
+            if(workerSet.size() < coreSize){
+                Worker worker = new Worker(task);
+                log.debug("新增 worker{}, {}", worker, task);
+                workerSet.add(worker);
+                worker.start();
+            }
+            else {
+    //            blockingQueue.put(task);
+                //自定义等待策略
+                //1. 死等
+                //2. 超时等待
+                //3. 抛出异常
+                //4. 放弃执行
+                //5. 调用者线程自己执行
+
+                rejectPolicy.reject(blockingQueue,task);
+            }
+        }
+    }
+
     class Worker extends Thread{
          Runnable task;
         Worker(Runnable task){
+
             this.task = task;
         }
 
